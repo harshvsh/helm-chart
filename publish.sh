5716bdf2896a703aa6bd7b0db761fba5baaa099c
David Peraza <dperaza@redhat.com>
2021-04-28 16:19:05 -0400

    Published charts 1619641011

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+helm package ../ssm/chart/ssm-service .
+helm repo index --url https://dperaza4dustbit.github.io/helm-chart/ .
+
+date_s="$(date +%s)"
+git add -A
+git commit -a -m "Published charts $date_s"
+git push origin main
