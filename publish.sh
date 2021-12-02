24f6782f923b9a92d640ddb7e0bdfc0c491132d5
David Peraza <dperaza@redhat.com>
2021-12-02 18:03:58 -0500

    Published charts 1638486238

--- publish.sh
+++ publish.sh
@@ -5,6 +5,7 @@ set -e
 helm package ../ssm/chart/psql-service
 helm package ../ssm/chart/psql-service-with-values
 helm package ../ssm/chart/ssm-service
+helm package ../ssm/chart/psql-service-dep
 
 helm repo index --url https://dperaza4dustbit.github.io/helm-chart/ .
 
