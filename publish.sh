b8d71ec2ef56aa94b11122bf2324ac5cb0c89ff3
David Peraza <dperaza@redhat.com>
2022-02-25 10:35:07 -0500

    Published charts 1645803307

--- publish.sh
+++ publish.sh
@@ -12,4 +12,4 @@ helm repo index --url https://dperaza4dustbit.github.io/helm-chart/ .
 date_s="$(date +%s)"
 git add -A
 git commit -a -m "Published charts $date_s"
-git push origin main
+git push -f origin main
